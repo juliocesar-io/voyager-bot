2422142912b3934d00c8e92f22873446536f7e92
Your Name <julioc255io@gmail.com>
2015-07-31 13:04:17 -0500

    add streaming camera

--- app.js
+++ app.js
@@ -1,25 +1,82 @@
-var app = require('http').createServer(handler),
-     io = require('socket.io').listen(app),
-     fs = require('fs'),
-     five = require("johnny-five");
+var express = require('express'),
+    app = express(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http),
+    fs = require('fs'),
+    five = require("johnny-five"),
+    path = require('path');
 
-// Start server
-app.listen(8000, function () {
-    console.log('Http server listening on port %d', 8000);
+var spawn = require('child_process').spawn;
+var proc;
+
+
+app.use('/', express.static(path.join(__dirname, 'stream')));
+
+http.listen(3000, function() {
+  console.log('Servidor escuchando en puerto 3000');
 });
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
 
-    res.writeHead(200);
-    res.end(data);
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/keypress.js', function(req, res) {
+  res.sendFile(__dirname + '/keypress.js');
+});
+
+var sockets = {};
+
+io.on('connection', function(socket) {
+
+  sockets[socket.id] = socket;
+  console.log("Clientes conectados ", Object.keys(sockets).length);
+
+  socket.on('disconnect', function() {
+    delete sockets[socket.id];
+
+    // no more sockets, kill the stream
+    if (Object.keys(sockets).length === 0) {
+      app.set('watchingFile', false);
+      if (proc) proc.kill();
+      fs.unwatchFile('./stream/image_stream.jpg');
+    }
   });
+
+  socket.on('start-stream', function() {
+    startStreaming(io);
+  });
+
+});
+
+function stopStreaming() {
+  if (Object.keys(sockets).length === 0) {
+    app.set('watchingFile', false);
+    if (proc) proc.kill();
+    fs.unwatchFile('./stream/image_stream.jpg');
+  }
 }
+
+function startStreaming(io) {
+
+  if (app.get('watchingFile')) {
+    io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
+    return;
+  }
+
+  var args = ["-w", "640", "-h", "480", "-o", "./stream/image_stream.jpg", "-t", "999999999", "-tl", "100"];
+  proc = spawn('raspistill', args);
+
+  console.log('Watching for changes...');
+
+  app.set('watchingFile', true);
+
+  fs.watchFile('./stream/image_stream.jpg', function(current, previous) {
+    io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
+});
+
+}
+
 
 board = new five.Board();
 

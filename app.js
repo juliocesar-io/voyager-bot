36a31aeb9d0eee9d3a465206af4e74b1dad73d98
Julio César <julioc255io@gmail.com>
2016-04-06 01:29:21 -0500

    servo init

--- app.js
+++ app.js
@@ -64,13 +64,13 @@ board.on("ready", function() {
 
  // SERVO
 
- var servo = new five.Servo(9);
+
 
  // Servo alternate constructor with options
- /*
+
  var servo = new five.Servo({
    id: "MyServo",     // User defined id
-   pin: 10,           // Which pin is it attached to?
+   pin: 9,           // Which pin is it attached to?
    type: "standard",  // Default: "standard". Use "continuous" for continuous rotation servos
    range: [0,180],    // Default: 0-180
    fps: 100,          // Used to calculate rate of movement between positions
@@ -81,12 +81,7 @@ board.on("ready", function() {
      speed: five.Servo.Continuous.speeds["@5.0V"]
    }
  });
- */
 
- // Add servo to REPL (optional)
- this.repl.inject({
-   servo: servo
- });
 
 
  // Servo API

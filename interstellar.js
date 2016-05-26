84b8ffbaf9a28b5649c0337680ea056de9398d3b
Julio César <julioc255io@gmail.com>
2016-05-26 16:44:27 -0500

    improve interface

--- interstellar.js
+++ interstellar.js
@@ -1,10 +1,19 @@
+var socket = io.connect();
 $("#volume").slider({
     min: 0,
     max: 255,
-    value: 100,
+    value: 127,
 		range: "min",
 		animate: true,
     slide: function(event, ui) {
-      console.log((ui.value));
+      speed = ui.value;
+      console.log((speed));
+      $("#speed").text(speed)
+      if (speed = 255) {
+
+      } else {
+        socket.emit('vel', speed);
+      }
+
     }
   });
